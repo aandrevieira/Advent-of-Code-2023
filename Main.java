f9c9cc218d262509b141980c058734088ee9451c
André Vieira <andregcv2431@gmail.com>
2023-12-02 06:53:14 +0000

    Challenge 2 part 2

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ public class Main {
             System.out.println("1. C1");
             System.out.println("2. C2");
             System.out.println("3. C3");
-            System.out.println("32. Exit");
+            System.out.println("42. Exit");
             System.out.print("Enter your choice: ");
 
             choice = scanner.nextInt();
@@ -38,16 +38,16 @@ public class Main {
                     if(choice2 == 1) {
                         C2P1(); 
                     } else if (choice2 ==2) {
-                        //C2P2();
+                        C2P2();
                     }
                     break;
-                case 32:
+                case 42:
                     System.out.println("Exiting...");
                     break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
-        } while (choice != 32);
+        } while (choice != 42);
 
         scanner.close();
     }
@@ -73,7 +73,7 @@ public class Main {
                 answer += Integer.valueOf(String.valueOf(num1) + String.valueOf(num2));
             }
             scanner.close();
-            System.out.println(answer);   
+            System.out.println("Your answer: " + answer);     
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -146,7 +146,7 @@ public class Main {
                 answer += Integer.valueOf(String.valueOf(num1) + String.valueOf(num2));
             }
             scanner.close();
-            System.out.println(answer);   
+            System.out.println("Your answer: " + answer);    
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -190,7 +190,46 @@ public class Main {
                 }
             }
             scanner.close();
-            System.out.println(answer);   
+            System.out.println("Your answer: " + answer);     
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+      }
+    }
+
+    public static void C2P2 () { //answer to challenge 2 part 1 was 66027
+        try{
+
+            Integer answer = 0;
+            File file = new File("C2file.txt");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+
+                String data = scanner.nextLine();
+                boolean possible = true;
+                Map<String, Integer> colorsMap = new HashMap<>();
+                colorsMap.put("red", -1);
+                colorsMap.put("green", -1);
+                colorsMap.put("blue", -1);
+
+                for(String color : colorsMap.keySet()){
+                    Pattern pattern = Pattern.compile("(\\d+) " + color);
+                    Matcher matcher = pattern.matcher(data);
+
+                    while(matcher.find()){
+                        int n = Integer.parseInt(matcher.group(1));
+                        if (n > colorsMap.get(color)){
+                            colorsMap.put(color, n);
+                        }
+                    }
+                }
+
+                answer +=  colorsMap.get("red") * colorsMap.get("green") * colorsMap.get("blue");
+            }
+
+            scanner.close();
+            System.out.println("Your answer: " + answer);   
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
